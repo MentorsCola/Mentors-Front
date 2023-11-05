c077c449dddf623454d8592e18abd16bab91f1b5
kkh061101 <kkh061101@naver.com>
2023-11-05 16:14:55 +0900

    initial commit

---
+++ src/app/layout.js
@@ -0,0 +1,17 @@
+import './globals.css'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'Mentors',
+  description: 'GSM 아이디어 페스티벌 프로젝트 Mentors',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
---
+++ src/app/page.js
@@ -0,0 +1,10 @@
+"use client"
+import * as S from './style';
+
+export default function Home() {
+  return (
+    <S.Home>
+      main
+    </S.Home>
+  )
+}
