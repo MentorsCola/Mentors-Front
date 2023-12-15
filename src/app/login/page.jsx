ed2993dad1a561152a38962b5a3b1964a4b39586
kkh061101 <kkh061101@naver.com>
2023-12-15 09:43:44 +0900

    signup

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -2,12 +2,14 @@
 import { useState } from 'react'
 import * as S from './style'
 import Link from 'next/link'
+import axios from 'axios'
 
 export default function Login() {
+  const url = ''
   const [email, setEmail] = useState('')
   const [pw, setPw] = useState('')
   const LoginFun = async e => {
-    // await axios
+    // await axios.post(`${url}/auth/login/`)
   }
   return <S.Back>
     <S.Main onSubmit={e => {
@@ -19,7 +21,7 @@ export default function Login() {
         로그인
       </S.Title>
       <S.Paragraph>이메일</S.Paragraph>
-      <S.Input onChange={e => setEmail(e.target.value)} value={email} />
+      <S.Input type='email' onChange={e => setEmail(e.target.value)} value={email} />
       <S.Paragraph>비밀번호</S.Paragraph>
       <S.Input onChange={e => setPw(e.target.value)} value={pw} />
       <S.Button>로그인</S.Button>
---
+++ src/app/signup/style.js
@@ -0,0 +1,100 @@
+import styled from "styled-components";
+
+export const Back = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100vw;
+  height: 100vh;
+`
+export const Main = styled.form`
+  box-shadow: 0 0 30px 1px gray;
+  background-color: white;
+  display: flex;
+  flex-direction: column;
+  border-radius: 5px;
+  padding: 25px ;
+  color: #505050;
+  `
+
+export const ExistAccount = styled.div`
+  display: flex;
+  margin-top: 20px;
+  font-size: 13px;
+  a{
+    color: #8ABF54;
+  }
+  span{
+    margin-right: 5px;
+  }
+`
+
+export const Title = styled.h1`
+  width: 100%;
+  font-size: 25px;
+  text-align: center;
+  margin-top: 30px;
+  margin-bottom: 60px;
+  `
+export const Paragraph = styled.p`
+  margin: 10px 0;
+`
+export const Input = styled.input`
+  color: #505050;
+  background-color: white;
+  border: none;
+  border-bottom: 1px solid black;
+  padding-left: 2px;
+  height: 30px;
+  color: black;
+  &:focus{
+    outline: none;
+  }
+`
+export const NickName = styled.div`
+  color: black;
+  background-color: #F1F1F1;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 10px 0;
+  font-size: 16px;
+  width: 320px;
+  border: none;
+  font-weight: bold;
+  margin-top: 20px;
+  border-radius: 2px;
+`
+
+export const RegenerateButton = styled.div`
+  position: absolute;
+  margin-left: 275px;
+  cursor: pointer;
+  @keyframes spin {
+    0%{
+      transform: rotate(0deg);
+    }
+    100%{
+      transform: rotate(360deg);
+    }
+  }
+  &:hover{
+    animation: spin 1s infinite;
+  }
+  transition: ease;
+`
+
+export const Button = styled.button`
+  background-color: #8ABF54;
+  padding: 10px 0;
+  cursor: pointer;
+  font-size: 16px;
+  font-weight: bold;
+  width: 320px;
+  border: none;
+  margin-top: 50px;
+  border-radius: 2px;
+  &:hover{
+    background-color: darken(#8ABF54, 10%);
+  }
+`
