a1c30132fcf95916a5d8bb69a239013c016e407d
kkh061101 <kkh061101@naver.com>
2023-12-15 11:42:56 +0900

    publishing

--- src/app/posting/page.jsx
+++ src/app/posting/page.jsx
@@ -1,3 +1,68 @@
+"use client"
+import { useState } from 'react';
+import * as S from './style';
+import Showdown from 'showdown';
+
 export default function Posting() {
-  return <></>
-}
+  const [title, setTitle] = useState('');
+  const [tag, setTag] = useState('');
+  const [tags, setTages] = useState([])
+  const [textarea, setTextarea] = useState('')
+  const DivideTags = e => {
+    console.log(e)
+  }
+  const PressKey = key => {
+    let press;
+    switch (key) {
+      case 'h1':
+        press = '\n# ';
+        break;
+      case 'h2':
+        press = '\n## ';
+        break;
+      case 'h3':
+        press = '\n### ';
+        break;
+      case 'h4':
+        press = '\n#### ';
+        break;
+    }
+    const current = document.querySelector('textarea').selectionStart;
+    const text = textarea.slice(0, current) + press + textarea.slice(current);
+    setTextarea(text);
+    document.querySelector('textarea').focus()
+    document.querySelector('textarea').setSelectionRange(current + press.length, current + textarea.length);
+  }
+  return <S.Background>
+    <S.PostBackground>
+      <S.TitleInput value={title} onChange={e => setTitle(e.target.value)} placeholder='제목을 입력해주세요' />
+      <S.DivideDiv />
+      <S.TagsInput placeholder='태그를 입력해주세요' onKeyDown={DivideTags} />
+      <S.AddButtons>
+        <S.AddButton onClick={e => PressKey('h1')}>H1</S.AddButton>
+        <S.AddButton onClick={e => PressKey('h2')}>H2</S.AddButton>
+        <S.AddButton onClick={e => PressKey('h3')}>H3</S.AddButton>
+        <S.AddButton onClick={e => PressKey('h4')}>H4</S.AddButton>
+        <S.DivideButton />
+        <S.AddButton>B</S.AddButton>
+        <S.AddButton>I</S.AddButton>
+        <S.AddButton>T</S.AddButton>
+        <S.DivideButton />
+        <S.AddButton>”</S.AddButton>
+        <S.AddButton><img /></S.AddButton>
+        <S.AddButton><img /></S.AddButton>
+        <S.AddButton><img /></S.AddButton>
+      </S.AddButtons>
+      <S.TextArea placeholder='본문을 적어주세요 . . .' onChange={e => setTextarea(e.target.value)} value={textarea} />
+      <S.BottomButtons>
+        <S.Exit>나가기</S.Exit>
+        <S.BorderButtons>
+          <S.BorderButton>삭제</S.BorderButton>
+          <S.BorderButton>업로드</S.BorderButton>
+        </S.BorderButtons>
+      </S.BottomButtons>
+    </S.PostBackground>
+    <S.PreviewBackground dangerouslySetInnerHTML={{ __html: new Showdown.Converter().makeHtml(textarea) }}>
+    </S.PreviewBackground>
+  </S.Background>
+} 
---
+++ src/app/posting/style.js
@@ -0,0 +1,148 @@
+import styled, { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  @font-face {
+    font-family: 'Pretendard-Regular';
+    src: url('https://cdn.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff') format('woff');
+    font-weight: 400;
+    font-style: normal;
+  }
+`
+
+export const Background = styled.div`
+  display: flex;
+  align-items: center;
+  /* justify-content: center; */
+`
+
+export const PostBackground = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 50vw;
+  height: 100vh;
+  border-right: 3px solid #D9D9D9;
+  padding: 50px;
+`
+
+export const PreviewBackground = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 50vw;
+  height: 100vh;
+  border-right: 3px solid #D9D9D9;
+  padding: 50px;
+`
+
+export const TitleInput = styled.input`
+  color: black;
+  background-color: #F5F5F5;
+  outline: none;
+  border: none;
+  font-size: 30px;
+  font-weight: bold;
+`
+export const DivideDiv = styled.div`
+  background-color: #D9D9D9;
+  width: 20vw;
+  height: 7px;
+  margin: 30px 0;
+`
+
+export const TagsInput = styled.input`
+  color: black;
+  background-color: #F5F5F5;
+  outline: none;
+  border: none;
+  font-size: 20px;
+  font-weight: bold;
+`
+
+export const AddButtons = styled.div`
+  display: flex;
+  align-items: center;
+  margin: 20px 0;
+  overflow-x: scroll;
+  overflow-y: hidden;
+  padding: 5px 0;
+  &::-webkit-scrollbar{
+    height: 5px;
+  }
+  &::-webkit-scrollbar-thumb{
+    background-color: #D9D9D9;
+  }
+`
+
+export const DivideButton = styled.div`
+  min-width: 3px;
+  min-height: 25px;
+  background-color: #D9D9D9;
+  margin: 0 10px;
+`
+
+export const AddButton = styled.button`
+  margin: 0 10px;
+  font-size: 18px;
+  cursor: pointer;
+  background-color: #F5F5F5;
+  color: #858E96;
+  border: none;
+  transition: ease 0.5s;
+  &:hover{
+    color: black;
+  }
+`
+
+export const TextArea = styled.textarea`
+  font-family: 'Pretendard-Regular';
+  height: 55vh;
+  font-size: 18px;
+  background-color: #F5F5F5;
+  color: black;
+  border: none;
+  resize: none;
+  &::-webkit-scrollbar{
+    width: 5px;
+  }
+  &::-webkit-scrollbar-thumb{
+    background-color: #D9D9D9;
+  }
+`
+
+export const BottomButtons = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-top: 10px;
+`
+
+export const Exit = styled.button`
+  margin: 0 10px;
+  font-size: 15px;
+  cursor: pointer;
+  background-color: #F5F5F5;
+  color: #858E96;
+  font-weight: bold;
+  padding: 3px 0;
+  width: 80px;
+  border: none;
+  outline: none;
+  &:hover{
+    color: black;
+  }
+`
+export const BorderButtons = styled.div`
+  display: flex;
+`
+
+export const BorderButton = styled.button`
+  margin: 0 10px;
+  font-size: 15px;
+  width: 80px;
+  cursor: pointer;
+  background-color: #F5F5F5;
+  color: #858E96;
+  border: 3px solid #858E96;
+  border-radius: 3px;
+  font-weight: bold;
+  padding: 3px 0;
+`
