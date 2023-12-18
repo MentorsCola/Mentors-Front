250ad8b3b3cbe9d93936afa1b15c69a741cc55bb
이아론 <aron0613@naver.com>
2023-12-18 15:42:47 +0900

    Add 404Page

---
+++ src/app/404/style.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const PageWrapper = styled.div`
+  margin: 0;
+  overflow: hidden;
+  width: 100vw;
+  height: 100vh;
+  background-color: #8abf54;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const IllerWrapper = styled.div``;
