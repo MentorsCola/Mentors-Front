ff7cc35516ab9e2b075f6551ebed67babe7e1d1a
이아론 <aron0613@naver.com>
2023-12-18 15:09:13 +0900

    add link in PostCard

--- src/components/PostCard/index.jsx
+++ src/components/PostCard/index.jsx
@@ -1,34 +1,37 @@
+import Link from "next/link";
 import * as S from "./style";
 
-export default function PostCard({ title, date, username, likes }) {
+export default function PostCard({ id, title, date, username, likes }) {
   return (
-    <S.PostCardWrapper>
-      <S.TopWrapper>
-        <S.Title>{title}</S.Title>
-        <S.Date>{date}</S.Date>
-      </S.TopWrapper>
-      <S.LowerWrapper>
-        <S.Username>{username}</S.Username>
-        <S.Like>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="18"
-            height="18"
-            viewBox="0 0 19 18"
-            fill="none"
-          >
-            <path
-              d="M16.0835 6.1875C16.0835 4.32375 14.5092 2.8125 12.5675 2.8125C11.1162 2.8125 9.86975 3.657 9.3335 4.86225C8.79725 3.657 7.55075 2.8125 6.09875 2.8125C4.1585 2.8125 2.5835 4.32375 2.5835 6.1875C2.5835 11.6025 9.3335 15.1875 9.3335 15.1875C9.3335 15.1875 16.0835 11.6025 16.0835 6.1875Z"
-              fill="black"
-              stroke="black"
-              stroke-width="1.5"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-            />
-          </svg>
-          {likes}
-        </S.Like>
-      </S.LowerWrapper>
-    </S.PostCardWrapper>
+    <Link href={`/reading/${id}`}>
+      <S.PostCardWrapper>
+        <S.TopWrapper>
+          <S.Title>{title}</S.Title>
+          <S.Date>{date}</S.Date>
+        </S.TopWrapper>
+        <S.LowerWrapper>
+          <S.Username>{username}</S.Username>
+          <S.Like>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="18"
+              height="18"
+              viewBox="0 0 19 18"
+              fill="none"
+            >
+              <path
+                d="M16.0835 6.1875C16.0835 4.32375 14.5092 2.8125 12.5675 2.8125C11.1162 2.8125 9.86975 3.657 9.3335 4.86225C8.79725 3.657 7.55075 2.8125 6.09875 2.8125C4.1585 2.8125 2.5835 4.32375 2.5835 6.1875C2.5835 11.6025 9.3335 15.1875 9.3335 15.1875C9.3335 15.1875 16.0835 11.6025 16.0835 6.1875Z"
+                fill="black"
+                stroke="black"
+                stroke-width="1.5"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+            </svg>
+            {likes}
+          </S.Like>
+        </S.LowerWrapper>
+      </S.PostCardWrapper>
+    </Link>
   );
 }
