718514025362c5c1122d52708a4fdcef193f6c21
이아론 <aron0613@naver.com>
2023-12-19 20:14:50 +0900

    Update box shadow

--- src/app/login/style.js
+++ src/app/login/style.js
@@ -6,20 +6,20 @@ export const Back = styled.div`
   justify-content: center;
   width: 100vw;
   height: 100vh;
-`
+`;
 export const Main = styled.form`
-  box-shadow: 0 0 30px 1px gray;
+  box-shadow: 0px 4px 30px 0px rgba(112, 144, 176, 0.12);
   background-color: white;
   display: flex;
   flex-direction: column;
   border-radius: 5px;
-  padding: 25px ;
+  padding: 25px;
   color: #505050;
-  a{
+  a {
     margin-top: 20px;
     font-size: 13px;
   }
-`
+`;
 
 export const Title = styled.h1`
   width: 100%;
@@ -27,10 +27,10 @@ export const Title = styled.h1`
   text-align: center;
   margin-top: 30px;
   margin-bottom: 60px;
-  `
+`;
 export const Paragraph = styled.p`
   margin: 10px 0;
-`
+`;
 export const Input = styled.input`
   color: #505050;
   background-color: white;
@@ -39,13 +39,13 @@ export const Input = styled.input`
   padding-left: 2px;
   height: 30px;
   color: black;
-  &:focus{
+  &:focus {
     outline: none;
   }
-`
+`;
 
 export const Button = styled.button`
-  background-color: #8ABF54;
+  background-color: #8abf54;
   padding: 10px 0;
   cursor: pointer;
   font-size: 16px;
@@ -54,7 +54,7 @@ export const Button = styled.button`
   border: none;
   margin-top: 50px;
   border-radius: 2px;
-  &:hover{
-    background-color: darken(#8ABF54, 10%);
+  &:hover {
+    background-color: darken(#8abf54, 10%);
   }
-`
+`;
--- src/app/signup/style.js
+++ src/app/signup/style.js
@@ -6,28 +6,28 @@ export const Back = styled.div`
   justify-content: center;
   width: 100vw;
   height: 100vh;
-`
+`;
 export const Main = styled.form`
-  box-shadow: 0 0 30px 1px gray;
+  box-shadow: 0px 4px 30px 0px rgba(112, 144, 176, 0.12);
   background-color: white;
   display: flex;
   flex-direction: column;
   border-radius: 5px;
-  padding: 25px ;
+  padding: 25px;
   color: #505050;
-  `
+`;
 
 export const ExistAccount = styled.div`
   display: flex;
   margin-top: 20px;
   font-size: 13px;
-  a{
-    color: #8ABF54;
+  a {
+    color: #8abf54;
   }
-  span{
+  span {
     margin-right: 5px;
   }
-`
+`;
 
 export const Title = styled.h1`
   width: 100%;
@@ -35,10 +35,10 @@ export const Title = styled.h1`
   text-align: center;
   margin-top: 30px;
   margin-bottom: 60px;
-  `
+`;
 export const Paragraph = styled.p`
   margin: 10px 0;
-`
+`;
 export const Input = styled.input`
   color: #505050;
   background-color: white;
@@ -47,13 +47,13 @@ export const Input = styled.input`
   padding-left: 2px;
   height: 30px;
   color: black;
-  &:focus{
+  &:focus {
     outline: none;
   }
-`
+`;
 export const NickName = styled.div`
   color: black;
-  background-color: #F1F1F1;
+  background-color: #f1f1f1;
   display: flex;
   align-items: center;
   justify-content: center;
@@ -64,28 +64,28 @@ export const NickName = styled.div`
   font-weight: bold;
   margin-top: 20px;
   border-radius: 2px;
-`
+`;
 
 export const RegenerateButton = styled.div`
   position: absolute;
   margin-left: 275px;
   cursor: pointer;
   @keyframes spin {
-    0%{
+    0% {
       transform: rotate(0deg);
     }
-    100%{
+    100% {
       transform: rotate(360deg);
     }
   }
-  &:hover{
+  &:hover {
     animation: spin 1s;
   }
   transition: ease;
-`
+`;
 
 export const Button = styled.button`
-  background-color: #8ABF54;
+  background-color: #8abf54;
   padding: 10px 0;
   cursor: pointer;
   font-size: 16px;
@@ -94,7 +94,7 @@ export const Button = styled.button`
   border: none;
   margin-top: 50px;
   border-radius: 2px;
-  &:hover{
-    background-color: darken(#8ABF54, 10%);
+  &:hover {
+    background-color: darken(#8abf54, 10%);
   }
-`
+`;
